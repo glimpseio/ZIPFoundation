7584b1037df73fccd7a30fbbf123488374c7e857
Marc Prud'hommeaux <marc@glimpse.io>
2020-10-06 22:45:08 -0400

    Update FileManager+ZIP.swift
    Merge of full PR for https://github.com/weichsel/ZIPFoundation/pull/187

--- Sources/ZIPFoundation/FileManager+ZIP.swift
+++ Sources/ZIPFoundation/FileManager+ZIP.swift
@@ -100,15 +100,17 @@ extension FileManager {
         }
         // Defer extraction of symlinks until all files & directories have been created.
         // This is necessary because we can't create links to files that haven't been created yet.
-        let sortedEntries = archive.sorted { (left, right) -> Bool in
-            switch (left.type, right.type) {
-            case (.symlink, .symlink): return true // https://github.com/weichsel/ZIPFoundation/issues/185
-            case (.directory, .file): return true
-            case (.directory, .symlink): return true
-            case (.file, .symlink): return true
-            default: return false
-            }
-        }
+        //let sortedEntries = archive.sorted { (left, right) -> Bool in
+        //    switch (left.type, right.type) {
+        //    case (.symlink, .symlink): return true // https://github.com/weichsel/ZIPFoundation/issues/185
+        //    case (.directory, .file): return true
+        //    case (.directory, .symlink): return true
+        //    case (.file, .symlink): return true
+        //    default: return false
+        //    }
+        //}
+        let sortedEntries = try archive.sortEntries(Array(archive.makeIterator()))
+        
         var totalUnitCount = Int64(0)
         if let progress = progress {
             totalUnitCount = sortedEntries.reduce(0, { $0 + archive.totalUnitCountForReading($1) })
@@ -323,5 +325,83 @@ public extension URL {
         // Ensure this URL is contained in the passed in URL
         let parentDirectoryURL = URL(fileURLWithPath: parentDirectoryURL.path, isDirectory: true).standardized
         return self.standardized.absoluteString.hasPrefix(parentDirectoryURL.absoluteString)
+    }
+}
+
+
+extension Archive {
+    /// Sorts and returns the provided entries in to the order they should be extract in. This will
+    /// sort directories first, then files, then symlinks.
+    ///
+    /// Directories and files are sorted in the order they are in the archive. Symlinks will be
+    /// sorted in the order they need to be extracted.
+    ///
+    /// - Parameters:
+    ///   - entries: The entries to sort
+    /// - Returns: The sorted entries.
+    /// - Throws: An error if the entry contains malformed content.
+    public func sortEntries(_ entries: [Entry]) throws -> [Entry] {
+        let sortedSymlinks = try sortSymblinks(in: entries)
+        let sortedFilesAndDirectories = sortFilesAndDirectories(in: entries)
+        return sortedFilesAndDirectories + sortedSymlinks
+    }
+
+    private func sortSymblinks(in entries: [Entry]) throws -> [Entry] {
+        return try entries
+            .lazy
+            .filter { entry in
+                entry.type == .symlink
+            }.map { entry -> (entry: Entry, destination: String) in
+                var destinationPath: String!
+                _ = try extract(entry, bufferSize: defaultReadChunkSize, skipCRC32: true) { data in
+                    guard let linkPath = String(data: data, encoding: .utf8) else {
+                        throw ArchiveError.invalidEntryPath
+                    }
+                    destinationPath = entry
+                        .path
+                        .split(separator: "/")
+                        .dropLast()
+                        .joined(separator: "/")
+                        + "/"
+                        + linkPath
+                }
+                return (entry, destinationPath)
+            }.reduce(into: [(entry: Entry, destination: String)]()) { entries, element in
+                let unsortedPath = element.entry.path
+                let unsortedDestination = element.destination
+
+                for (index, sortedElement) in entries.enumerated() {
+                    let sortedPath = sortedElement.entry.path
+                    let sortedDestination = sortedElement.destination
+
+                    if unsortedDestination.hasPrefix(sortedDestination) {
+                        entries.insert(element, at: entries.index(after: index))
+                        return
+                    } else if sortedDestination.hasPrefix(unsortedDestination) {
+                        entries.insert(element, at: index)
+                        return
+                    } else if sortedDestination.hasPrefix(unsortedPath) {
+                        entries.insert(element, at: index)
+                        return
+                    } else if unsortedDestination.hasPrefix(sortedPath) {
+                        entries.insert(element, at: entries.index(after: index))
+                        return
+                    }
+                }
+
+                entries.append(element)
+            }.map { $0.entry }
+    }
+
+    private func sortFilesAndDirectories(in entries: [Entry]) -> [Entry] {
+        return entries
+            .filter { entry in
+                entry.type != .symlink
+            }.sorted { (left, right) -> Bool in
+                switch (left.type, right.type) {
+                case (.file, .directory): return false
+                default: return true
+                }
+            }
     }
 }
